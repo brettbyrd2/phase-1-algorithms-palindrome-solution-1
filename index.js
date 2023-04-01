50f3e1af4bf6dcb3e42f7b9a8a0e11997eea2770
Brett Byrd <byrdb2@yahoo.com>
2023-04-01 16:25:45 -0500

    complete

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function reverse(word) {
+  // 'abc' => 'cba'
+const wordArray = word.split("")
+const reversedWordArray = wordArray.reverse()
+const reversedWord = reversedWordArray.join("")
+return reversedWord
 }
+
+
+function isPalindrome(word) {
+ const reversedWord = reverse(word)
+
+  return word === reversedWord 
+  }
 
 /* 
   Add your pseudocode here
+  if the word is the same in reverse return true
+ 
+  reverse the input string
+
+  if the input is the same as the reversed input
+  return true
+  else
+  return false
+
 */
 
 /*
